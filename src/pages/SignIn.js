21f7733aa3d7ed2660322ca38566fe8228bdc9dc
Saumya Bhatt <saumya.bhatt106@gmail.com>
2024-12-28 23:39:29 +0530

    Handled signin

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 import { Box, Button, Input, Typography } from "@mui/joy";
 import { CreateUserRequest } from 'karya-client/client/requests'
+import PopupStack from "../components/PopupStack";
+
 
 function SignIn({ client, setUser }) {
     const [username, setUsername] = useState("");
+    const [popups, setPopups] = useState([]);
+
+    const addPopup = (message, type) => {
+        setPopups((prev) => [...prev, { message, type }]);
+    };
+
+    const removePopup = (index) => {
+        setPopups((prev) => prev.filter((_, i) => i !== index));
+    };
 
     const handleSignIn = async () => {
         if (username.trim()) {
@@ -12,7 +23,13 @@ function SignIn({ client, setUser }) {
                 user = await client.getUser(username.trim());
             } catch (error) {
                 console.log("User does not exist. Creating a new one");
-                user = await client.createUser(new CreateUserRequest(username.trim()));
+                try {
+                    user = await client.createUser(new CreateUserRequest(username.trim()));
+                } catch (error) {
+                    addPopup("Unable to connect to client: ", error)
+                    return
+                }
+
             }
 
             console.log(user); // This will now log the resolved user object
@@ -34,6 +51,7 @@ function SignIn({ client, setUser }) {
                 gap: "16px",
             }}
         >
+            <PopupStack popups={popups} onRemove={removePopup} />
             <Typography level="h2">Create plans as user</Typography>
             <Typography level="body-sm">Or login as an existing user</Typography>
             <Input
